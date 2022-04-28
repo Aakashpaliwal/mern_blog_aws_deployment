161e98fde115aa0761b278dc1434edb288e7782e
unknown <a.paliwal@thesynapses.com>
2022-04-28 17:40:01 +0530

    CORS error resolve

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ limit: "300mb", extended: true }));
 app.use(express.json({ limit: "300mb" }));
 // app.use(cors());
 app.use(cors(), function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Origin", "https://venerable-mermaid-74b042.netlify.app/"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
